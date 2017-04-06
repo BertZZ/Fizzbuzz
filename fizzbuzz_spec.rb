0f9a5ce63fd497a53a8e283a5b97ef8b2684df08
BertZZ <bertie.wooles@hotmail.co.uk>
2017-04-06 17:18:26 +0000

    Added 15 test

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -7,11 +7,19 @@ describe 'Fizzbuzz' do
      it 'knows if a number is divisable by 5' do
          expect(is_divisible_by_five?(5)).to be_truthy
      end
-    it 'knows if a number is not divisible by 3' do
+     it 'knows if a number is divisable by 15' do
+         expect(is_divisible_by_fifteen?(15)).to be_truthy
+     end
+         
+     it 'knows if a number is not divisible by 3' do
          expect(is_divisible_by_three?(1)).not_to be_truthy
      end
      
      it 'knows if a number is not divisible by 5' do
          expect(is_divisible_by_five?(1)).not_to be_truthy
+     end
+     
+     it 'knows if a number is not divisable by 15' do
+         expect(is_divisible_by_fifteen?(1)).not_to be_truthy
      end
 end
