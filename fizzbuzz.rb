ffadc50d2df150289aea7c042d912678c35f874f
BertZZ <bertie.wooles@hotmail.co.uk>
2017-04-06 17:18:50 +0000

    Passed 15 test

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,5 +3,10 @@ def is_divisible_by_three?(number)
 end
 
 def is_divisible_by_five?(number)
-    true
+    number % 5 == 0 
 end
+
+def is_divisible_by_fifteen?(number)
+    number % 15 == 0
+end
+    
