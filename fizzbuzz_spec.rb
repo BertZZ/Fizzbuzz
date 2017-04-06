14ca7eaca2eb39f2aa4fe519de59816c3f20e780
BertZZ <bertie.wooles@hotmail.co.uk>
2017-04-06 15:37:13 +0000

    New test added to spec

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -1,9 +1,10 @@
 require './fizzbuzz'
 
 describe 'Fizzbuzz' do 
-    context 'knows that a number is divisible by' do
-     it '3' do
-         expect(is_divisible_by_three(3)).to be_truthy
+     it 'knows if a number is divisable by 3' do
+         expect(is_divisible_by_three?(3)).to be_truthy
      end
-    end
+     it 'knows if a number is not divisible by 3' do
+         expect(is_divisible_by_three?(1)).not_to be_truthy
+     end
 end
