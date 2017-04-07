ad29c02e90b9e942a4ce0f4c074364d2c6c10a60
BertZZ <bertie.wooles@hotmail.co.uk>
2017-04-07 16:36:27 +0000

    Added Final Test and passing code

--- Fizzbuzz_method_2/fizzbuzz.rb
+++ Fizzbuzz_method_2/fizzbuzz.rb
@@ -1,5 +1,7 @@
 def fizzbuzz(number)
-    if number % 5 ==0 
+    if number % 3 == 0 && number % 5 ==0
+        return "FizzBuzz"
+    elsif number % 5 ==0 
         return 'Buzz'
     elsif number % 3 == 0
         return 'Fizz'
--- Fizzbuzz_method_2/fizzbuzz_spec.rb
+++ Fizzbuzz_method_2/fizzbuzz_spec.rb
@@ -5,12 +5,20 @@ describe 'fizzbuzz' do
         expect(fizzbuzz(3)).to eq 'Fizz'
     end
     
+    it 'returns "Fizz" when passed 6' do 
+        expect(fizzbuzz(6)).to eq 'Fizz' 
+    end
+    
      it 'returns 4 when passed 4' do
         expect(fizzbuzz(4)).to eq 4
     end
     
     it 'returns "Buzz" when passed 5' do
         expect(fizzbuzz(5)).to eq 'Buzz'
+    end
+    
+    it 'returns "FizzBuzz" when passed 15' do
+        expect(fizzbuzz(15)).to eq 'FizzBuzz'
     end
         
 end
